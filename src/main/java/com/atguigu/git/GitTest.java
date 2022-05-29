ce72077899120013c9c97818395f1f203a34f382
adld <adld@qq.com>
2022-05-29 19:49:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author adld
+ * @date 2022/5/29 - 19:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
